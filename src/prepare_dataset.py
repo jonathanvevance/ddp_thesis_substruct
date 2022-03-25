f40758f60797b005dda3ae70357b950b180da270
Jonathan Ve Vance <jonathanvevance@gmail.com>
2022-03-25 16:42:15 +0530

    file structure changes

--- src/prepare_dataset.py
+++ src/prepare_dataset.py
@@ -1,3 +1,11 @@
+"""Python file to make dataset structures."""
+
+import os
+
+from substructures.get_substructures import *
+
+SUBSTRUCTURE_KEYS = 'MACCS'
+
 # DATA PART
 
 # get substructure match tuples - CAN DO
@@ -12,3 +20,11 @@
 #       a. Have multi hot vectors (for atoms) for each substructure match
 #       b. Get R1 Readout for substructure representation.
 #       c. Get R2 Readout for pair of substructures -> [0, 1]
+
+
+if __name__ == '__main__':
+    print(os.path.exists("data/raw/train.txt"))
+
+    rxn_dataset = []
+    for data in rxn_dataset:
+        pass
